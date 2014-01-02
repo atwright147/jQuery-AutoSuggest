7c4a4328d2c1ddea56be050dfb489434c2328224
Andy Wright <atwright147@gmail.com>
2014-01-02 01:18:14 +0000

    more edits :)

--- jquery.autosuggest.js
+++ jquery.autosuggest.js
@@ -37,7 +37,6 @@
 			'restful'   : true,
 			'dataType'  : null,  // intelligent auto-guess 
 			'parse'     : function(data) {
-								console.debug('parse:',data);
 								return data;
 						  },
 			'template'  : function(data) {
@@ -49,13 +48,14 @@
 						  },
 			'onSelect'  : function(el) {
 								var $el = $(el);
+								var $parent = $el.parents('div');
 								var selected_id = $el.data('id');
-								console.info(selected_id);
 								$.event.trigger({
 									type: 'autosuggest:selection',
 									value: selected_id
 								});
-								$el.parents('div').empty().hide();
+								$parent.empty().hide();
+								$('input[type="hidden"]#'+$parent.data('original-id')).val(selected_id);
 						  }
 		}, options);
 
@@ -63,27 +63,34 @@
 
 		return this.each(function() {
 			var $this = $(this);
-			console.log($this);
-			var id = $this.attr('id');
+			var id  = $this.attr('id');
+			var _id = '_'+id;
 			var offset = $this.offset();
 			var left = offset.left;
+
+			// change name attr to add an underscore at the start
+			var name = $this.attr('name');
+			$this.attr('name', '_'+name);
+			// create hidden input with original name (no underscore)
+			var $hidden = $('<input type="hidden" name="'+name+'" id="'+id+'" value="">');
+			$this.after($hidden);
 
 			if ($this.height() == 0) { var height = 30;  } else { var height = $(this).height() + 5; }
 			if ($this.width()  == 0) { var width  = 200; } else { var width  = $this.width() + 5; }
 
 			var top = offset.top + height;
 
-			var $suggestions = $('<div class="autosuggest-suggestions" id="autosuggest_'+id+'" style="position: absolute; left: '+left+'px; top: '+top+'px; min-width: '+width+'px; display: none;"/>');
+			var $suggestions = $('<div class="autosuggest-suggestions" id="autosuggest_'+id+'" style="position: absolute; left: '+left+'px; top: '+top+'px; min-width: '+width+'px; display: none;" data-original-id="'+id+'"/>');
 			$($this).after($suggestions);
 
-			$(this).keyup(function() {
+			$(this).keyup(function(e) {
 				if ($('#'+id).val().length >= o.minLength) {
 					var query = $('#'+id).val();
-					var url = $('#'+id).data('url');
+					var url   = $('#'+id).data('url');
 					if (o.restful && url.substring(url.length-1) == "/") {
 						//console.debug(o.restful);
-						//url = url.substring(0, url.length-1) + '/' + query;
 						url = url.substring(0, url.length-1);
+						//url = url.substring(0, url.length-1) + '/' + query;
 					}
 					if ( ! o.restful) {
 						//url = url + query;
